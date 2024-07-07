f04ac7dcfd607e8f60dd8fde881054b8ff3834e9
SMNoushan <noushan.md@gmail.com>
2024-07-07 20:23:02 +0600

    added db.post  save room data

--- index.js
+++ index.js
@@ -98,6 +98,13 @@ async function run() {
       res.send(result);
     });
 
+    // save new room data
+    app.post("/room", async (req, res) => {
+      const roomData = req.body;
+      const result = await roomCollection.insertOne(roomData);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
